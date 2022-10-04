ce69c92853e5dd98040f26b7cea0853dd9599c2b
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-10-04 10:45:28 +0300

    webpack rules

--- webpack.rules.js
+++ webpack.rules.js
@@ -10,6 +10,45 @@ module.exports = [
       }
     }
   },
+  {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|mp3)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
   // Put your webpack loader rules in this array.  This is where you would put
   // your ts-loader configuration for instance:
   /**
